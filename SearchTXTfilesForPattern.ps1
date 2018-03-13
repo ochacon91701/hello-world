f4af2ac34f1c9a533f2fe991139e0996915395db
ochacon91701 <37346843+ochacon91701@users.noreply.github.com>
2018-03-13 12:41:11 -0700

    Add files via upload

---
+++ SearchTXTfilesForPattern.ps1
@@ -0,0 +1,31 @@
+﻿
+
+$Path = "C:\SQLExports\arsqlcs\DST17000SanBernardinoTraining\2018-03-12_0804\StoredProcedures"
+$Text = "edulink"
+$PathArray = @()
+$Results = "C:\temp\test.txt"
+$count = 0
+$ext = "*.sql"
+clear
+
+
+Get-ChildItem $Path -Filter $ext |
+Where-Object { $_.Attributes -ne "Directory"} |
+ForEach-Object {
+    If (Get-Content $_.FullName | Select-String -Pattern $Text) {
+        $PathArray += $_.FullName
+
+    }
+}
+Write-Host "Searching filepath: $Path\$ext "
+Write-Host "Pattern: $Text "
+
+
+Write-Host "Matches:"
+    $PathArray | ForEach-Object {
+    $count++
+    $count.tostring() + ") " + $_
+}
+
+
+
